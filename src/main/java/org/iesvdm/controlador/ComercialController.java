f02eb4c8eb05015ae075ca817a25e5223bf46661
Guillermo <guille@example.com>
2024-01-11 11:50:20 +0100

    init

---
+++ src/main/java/org/iesvdm/controlador/ComercialController.java
@@ -0,0 +1,26 @@
+package org.iesvdm.controlador;
+
+import org.iesvdm.modelo.Cliente;
+import org.iesvdm.modelo.Comercial;
+import org.iesvdm.service.ComercialService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+
+public class ComercialController {
+
+    private ComercialService comercialService;
+
+    @GetMapping("/comerciales") //Al no tener ruta base para el controlador, cada método tiene que tener la ruta completa
+    public String listar(Model model) {
+
+        List<Comercial> listaComerciales =  comercialService.listAll();
+        model.addAttribute("listaComerciales", listaComerciales);
+
+        return "comerciales";
+    }
+}
